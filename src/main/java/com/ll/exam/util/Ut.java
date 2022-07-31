ce5d4a17c9f7189dbef32fcfa3f3e4e10bc43ed9
jinsu291 <wlstn291@naver.com>
2022-08-01 01:43:40 +0900

    array 형태의 json으로부터 List<ArticleDto> 타입의 객체 얻기>

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -1,11 +1,13 @@
 package com.ll.exam.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.List;
 
 public class Ut {
-    public static class json{
+    public static class json {
         private static final ObjectMapper om;
 
         static {
@@ -20,9 +22,17 @@ public class Ut {
             }
         }
 
-        public static <T> T toObj(String jsonStr, Class cls, T defaultValue) {
+        public static <T> T toObj(String jsonStr, Class<T> cls, T defaultValue) {
             try {
-                return (T) om.readValue(jsonStr, cls);
+                return om.readValue(jsonStr, cls);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> List<T> toObj(String jsonStr, TypeReference<List<T>> typeReference, List<T> defaultValue) {
+            try {
+                return om.readValue(jsonStr, typeReference);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
