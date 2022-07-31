cb1786591d39241ed26e96513fe711a93fdb61c3
jinsu291 <wlstn291@naver.com>
2022-08-01 01:22:27 +0900

    리스트와 맵을 json화

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,26 +1,52 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    void assertJ__assertThat(){
+    void assertJ__assertThat() {
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
     }
 
     @Test
-    void ObjectMapper__objToJsonStr() {
+    void ObjectMapper__articleDtoToJsonStr() {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
 
         String jsonStr = Ut.json.toStr(articleDto, "");
         assertThat(jsonStr).isNotBlank();
         assertThat(jsonStr).isEqualTo("""
                 {"id":1,"title":"제목","body":"내용"}
+                """.trim());
+    }
+
+    @Test
+    void ObjectMapper__articleDtoListToJsonStr() {
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
+        articleDtos.add(new ArticleDto(2, "제목2", "내용2"));
+
+        String jsonStr = Ut.json.toStr(articleDtos, "");
+        assertThat(jsonStr).isEqualTo("""
+                [{"id":1,"title":"제목1","body":"내용1"},{"id":2,"title":"제목2","body":"내용2"}]
+                """.trim());
+    }
+
+    @Test
+    void ObjectMapper__articleDtoMapToJsonStr() {
+        Map<String, ArticleDto> articleDtoMap = new HashMap<>();
+        articleDtoMap.put("가장오래된", new ArticleDto(1, "제목1", "내용1"));
+        articleDtoMap.put("최신", new ArticleDto(2, "제목2", "내용2"));
+        String jsonStr = Ut.json.toStr(articleDtoMap, "");
+        assertThat(jsonStr).isEqualTo("""
+                {"가장오래된":{"id":1,"title":"제목1","body":"내용1"},"최신":{"id":2,"title":"제목2","body":"내용2"}}
                 """.trim());
     }
 
